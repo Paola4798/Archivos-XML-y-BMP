c29731d1905f87e55d6c5cf174cac12265a5e63e
Paola4798 <36242948+Paola4798@users.noreply.github.com>
2018-02-28 16:11:09 -0600

    Add files via upload

---
+++ bmp y xml/bmp y xml/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace bmp_y_xml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenBmp = new System.Windows.Forms.Button();
+            this.lblAgenda = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblDomicilio = new System.Windows.Forms.Label();
+            this.lblLdN = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDomicilio = new System.Windows.Forms.TextBox();
+            this.txtLdN = new System.Windows.Forms.TextBox();
+            this.btnXML = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.txtBmp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenBmp
+            // 
+            this.btnOpenBmp.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenBmp.Location = new System.Drawing.Point(205, 34);
+            this.btnOpenBmp.Name = "btnOpenBmp";
+            this.btnOpenBmp.Size = new System.Drawing.Size(110, 51);
+            this.btnOpenBmp.TabIndex = 1;
+            this.btnOpenBmp.Text = "Datos";
+            this.btnOpenBmp.UseVisualStyleBackColor = true;
+            this.btnOpenBmp.Click += new System.EventHandler(this.btnOpenBmp_Click);
+            // 
+            // lblAgenda
+            // 
+            this.lblAgenda.AutoSize = true;
+            this.lblAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAgenda.Location = new System.Drawing.Point(378, 15);
+            this.lblAgenda.Name = "lblAgenda";
+            this.lblAgenda.Size = new System.Drawing.Size(71, 20);
+            this.lblAgenda.TabIndex = 2;
+            this.lblAgenda.Text = "Agenda";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(394, 52);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(67, 16);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(412, 81);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(49, 16);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(387, 106);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(74, 16);
+            this.lblTelefono.TabIndex = 5;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // lblDomicilio
+            // 
+            this.lblDomicilio.AutoSize = true;
+            this.lblDomicilio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDomicilio.Location = new System.Drawing.Point(384, 136);
+            this.lblDomicilio.Name = "lblDomicilio";
+            this.lblDomicilio.Size = new System.Drawing.Size(77, 16);
+            this.lblDomicilio.TabIndex = 6;
+            this.lblDomicilio.Text = "Domicilio:";
+            // 
+            // lblLdN
+            // 
+            this.lblLdN.AutoSize = true;
+            this.lblLdN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLdN.Location = new System.Drawing.Point(306, 161);
+            this.lblLdN.Name = "lblLdN";
+            this.lblLdN.Size = new System.Drawing.Size(155, 16);
+            this.lblLdN.TabIndex = 7;
+            this.lblLdN.Text = "Lugar de Nacimiento:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(467, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(244, 20);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(467, 77);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(244, 20);
+            this.txtEdad.TabIndex = 9;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(467, 102);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(244, 20);
+            this.txtTelefono.TabIndex = 10;
+            // 
+            // txtDomicilio
+            // 
+            this.txtDomicilio.Location = new System.Drawing.Point(467, 132);
+            this.txtDomicilio.Name = "txtDomicilio";
+            this.txtDomicilio.Size = new System.Drawing.Size(244, 20);
+            this.txtDomicilio.TabIndex = 11;
+            // 
+            // txtLdN
+            // 
+            this.txtLdN.Location = new System.Drawing.Point(467, 161);
+            this.txtLdN.Name = "txtLdN";
+            this.txtLdN.Size = new System.Drawing.Size(244, 20);
+            this.txtLdN.TabIndex = 12;
+            // 
+            // btnXML
+            // 
+            this.btnXML.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXML.Location = new System.Drawing.Point(692, 196);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(102, 33);
+            this.btnXML.TabIndex = 13;
+            this.btnXML.Text = "Save";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "ofd";
+            // 
+            // txtBmp
+            // 
+            this.txtBmp.Location = new System.Drawing.Point(12, 21);
+            this.txtBmp.Multiline = true;
+            this.txtBmp.Name = "txtBmp";
+            this.txtBmp.Size = new System.Drawing.Size(189, 212);
+            this.txtBmp.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 245);
+            this.Controls.Add(this.txtBmp);
+            this.Controls.Add(this.btnXML);
+            this.Controls.Add(this.txtLdN);
+            this.Controls.Add(this.txtDomicilio);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblLdN);
+            this.Controls.Add(this.lblDomicilio);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblAgenda);
+            this.Controls.Add(this.btnOpenBmp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpenBmp;
+        private System.Windows.Forms.Label lblAgenda;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblDomicilio;
+        private System.Windows.Forms.Label lblLdN;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDomicilio;
+        private System.Windows.Forms.TextBox txtLdN;
+        private System.Windows.Forms.Button btnXML;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.TextBox txtBmp;
+    }
+}
+
---
+++ bmp y xml/bmp y xml/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace bmp_y_xml
+{
+    public partial class Form1 : Form
+    {
+        char primerbit, segundobit;
+        int alto, ancho, bytes, bytesxp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool esArchivoBMP(FileStream archivo)
+        {
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(0, SeekOrigin.Begin);
+            //Se necesita para base de referenicia: orgin //Empezar 
+            primerbit = Convert.ToChar(br.ReadByte());
+            br.BaseStream.Seek(1, SeekOrigin.Begin);
+            segundobit = Convert.ToChar(br.ReadByte());
+            if (primerbit == 'B' && segundobit == 'M')
+            {
+                //Console.WriteLine("Marca del fichero: {0}{1}", primerbit, segundobit);
+                //txtBmp.Text += "es bmp";
+                return true;
+            }
+            else
+            {
+                br.Close();
+                archivo.Close();
+                //Console.WriteLine("no es bmp");
+                txtBmp.Text += "no es bmp";
+                return false;
+            }
+        }
+        private int tamaño(FileStream archivo)
+        {
+
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(2, SeekOrigin.Begin);
+            //2da posición en un archivo bmp para el tamaño de bytes
+            bytes = br.ReadInt32();
+            //Console.WriteLine("Tamaño real en bytes: " + bytes);
+            txtBmp.Text += "Tamaño real en bytes: " + bytes;
+            return bytes;
+        }
+
+
+        private int Ancho_y_Altura_Imagen(FileStream archivo)
+        {
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(18, SeekOrigin.Begin);
+            //18 posición en un archivo bmp para el ancho de una imagen
+            ancho = br.ReadInt32();
+            //Console.WriteLine("Ancho:{0} ", ancho);
+            txtBmp.Text += Environment.NewLine + "Ancho de la imagen: " + ancho;
+            alto = br.ReadInt32();
+            //Console.WriteLine("Alto:{0} ", alto);
+            txtBmp.Text += Environment.NewLine + "Alto de la imagen: " + alto;
+            return ancho + alto;
+
+        }
+        private int bits_por_Pixel(FileStream archivo)
+        {
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(28, SeekOrigin.Begin);
+            //28 posición en un archivo bmp para el bits por pixel
+            bytesxp = br.ReadInt16();
+            //Console.WriteLine("Bits por pixel:{0} ", bytesxp);
+            txtBmp.Text += Environment.NewLine + "Bits por pixel: " + bytesxp;
+            return bytesxp;
+        }
+
+
+
+        private void btnOpenBmp_Click(object sender, EventArgs e)
+        {
+            ofd.ShowDialog();
+            FileStream archivo = new FileStream(ofd.FileName, FileMode.Open);
+
+
+            if (esArchivoBMP(archivo) == true)
+            {
+                tamaño(archivo);
+                Ancho_y_Altura_Imagen(archivo);
+                bits_por_Pixel(archivo);
+            }
+            archivo.Close();
+        }
+
+        public void CrearXml(XmlDocument doc, string nombre, string Edad, string Telefono, string Domicilio, string Lugar_de_nacimieto)
+        {
+            XmlDeclaration documento = doc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            XmlElement element = doc.DocumentElement;
+            doc.InsertBefore(documento, element);
+            
+
+            XmlNode node = doc.CreateElement("Agenda");
+            doc.AppendChild(node);
+            XmlNode usNode = doc.CreateElement("Registros");
+            XmlAttribute name = doc.CreateAttribute("Nombre");
+            name.Value = nombre; 
+            usNode.Attributes.Append(name);
+            XmlAttribute age = doc.CreateAttribute("Edad");
+            age.Value = Edad;
+            usNode.Attributes.Append(age);
+            XmlAttribute telefono = doc.CreateAttribute("Telefono");
+            telefono.Value = Telefono;
+            usNode.Attributes.Append(telefono);
+            XmlAttribute domicilio = doc.CreateAttribute("Domicilio");
+            domicilio.Value = Domicilio;
+            usNode.Attributes.Append(domicilio);
+            XmlAttribute Lugar = doc.CreateAttribute("Lugar de nacimiento");
+            Lugar.Value = Lugar_de_nacimieto;
+            usNode.Attributes.Append(Lugar);
+            node.AppendChild(usNode);
+        }
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            sfd.Filter = ".xml|*.xml";
+            sfd.ShowDialog();
+            XmlDocument xml = new XmlDocument();
+
+            CrearXml(xml, txtNombre.Text, txtEdad.Text, txtTelefono.Text, txtDomicilio.Text, txtLdN.Text);
+            txtDomicilio.Clear();
+            txtEdad.Clear();
+            txtLdN.Clear();
+            txtNombre.Clear();
+            txtTelefono.Clear();
+
+          xml.Save(sfd.FileName);
+        }
+
+    }
+
+}
